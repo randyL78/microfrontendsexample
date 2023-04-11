339a0e2c07f1000d43b6a53a329d598b14ec7f9b
Randy Layne <randy.layne@crowdstreet.com>
2023-04-11 14:35:45 -0400

    Add header to container

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,6 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, '../src/components')
+    }
+  },
   module: {
     rules: [
       {
