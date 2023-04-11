1389b468f6b0a92a1ac95412affe337562b79fb2
Randy Layne <randy.layne@crowdstreet.com>
2023-04-11 09:48:39 -0400

    Create container deploy script

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,8 +1,24 @@
 const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const prodConfig = {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      shared: packageJson.dependencies,
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      }
+    })
+  ]
 };
 
 module.exports = merge(commonConfig, prodConfig);
